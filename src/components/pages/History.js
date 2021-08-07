4a52a75e7e515133a0aabaaefe67f8b65770d6e5
earthboxer <earthboxer@gmail.com>
2021-08-07 16:10:20 +0530

    third commit

--- src/components/pages/History.js
+++ src/components/pages/History.js
@@ -8,7 +8,7 @@ function History() {
         return Math.floor(Math.random() * (max - min + 1) + min)
     }
     useEffect(() => {
-        db.collection('history2').orderBy("date","desc").orderBy("time","desc").onSnapshot(snapshot => {
+        db.collection('history2').orderBy("date", "desc").orderBy("time", "desc").onSnapshot(snapshot => {
             sethistory(snapshot.docs.map(doc => doc.data()))
         })
 
@@ -18,20 +18,23 @@ function History() {
             <h1 className="history-header">TRANSACTION HISTORY</h1>
             <table className="tble "  >
                 <tbody>
-                    <tr key={randomIntFromInterval(90000000, 1000000000)}><th>date</th>
+                    <tr key={randomIntFromInterval(90000000, 1000000000)}>
+                        <th>date</th>
+                        <th>time</th>
                         <th>from</th>
                         <th>to</th>
-                        <th>money</th>
-                        <th>time</th></tr>
+                        <th>money</th></tr>
+
                     {history.map((item) => {
 
                         return (
                             <tr key={item.time}>
                                 <td>{item.date}</td>
+                                <td>{item.time}</td>
                                 <td>{item.from}</td>
                                 <td>{item.to}</td>
                                 <td>{item.money}</td>
-                                <td>{item.time}</td>
+
                             </tr>)
 
 
