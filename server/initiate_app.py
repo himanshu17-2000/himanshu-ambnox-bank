6d120f4a3094c5731896d68f86ebfa045966159f
himanshu17-2000 <earthboxer@gmail.com>
2023-08-03 15:19:47 +0530

    Front end api working

--- server/initiate_app.py
+++ server/initiate_app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from api import api
+from flask_cors import CORS
 from dotenv import load_dotenv
 from initiate_packages import db
 import os 
@@ -15,6 +16,7 @@ def createApp(testing):
         app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
     db.init_app(app)
+    CORS(app)
     app.register_blueprint(api)
     with app.app_context(): 
         db.create_all()
