994ceaba4bc97a4e139200686c84c5c08df151de
himanshu17-2000 <earthboxer@gmail.com>
2023-08-18 12:10:30 +0530

    All done

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,8 @@ import Home from './components/pages/Home';
 import Users from './components/pages/Users';
 import History from './components/pages/History';
 import Register from './components/pages/Register';
-import Demo from './components/pages/Demo';
-import Deposite from './components/pages/Deposite';
+import UserTransfer from './components/pages/UserTransfer'
+import DepositeAndWithdraw from './components/pages/DepositeAndWithdraw';
 import Login from './components/pages/Login';
 import Error from './components/pages/Error';
 
@@ -18,9 +18,9 @@ function App() {
         
         <Navbar />
         <Switch>
-          <Route exact path="/home/user/:transfer" component={Demo} />
+          <Route exact path="/home/user/:transfer" component={UserTransfer} />
           <Route exact path="/home/users" component={Users} />
-          <Route exact path="/home/depositeandwithdraw" component={Deposite} />
+          <Route exact path="/home/depositeandwithdraw" component={DepositeAndWithdraw} />
           <Route exact path="/home/history" component={History} />
           <Route exact path="/home/register" component={Register} />
           <Route exact path="/home" component={Home} />
